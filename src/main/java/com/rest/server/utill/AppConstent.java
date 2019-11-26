e8f1817a8ece2123a3b98ad89a6c724b0584bda9
tharakacz <tharakacz823@gmail.com>
2019-11-26 19:46:16 +0530

    Logic Replace

--- src/main/java/com/rest/server/utill/AppConstent.java
+++ src/main/java/com/rest/server/utill/AppConstent.java
@@ -1,4 +1,9 @@
 package com.rest.server.utill;
 
 public class AppConstent {
+  public final static String ROLEADMIN = "ADMIN";
+  public final static String ROLECLIENT = "CLIENT";
+  public final static String PENDING = "PENDING";
+  public final static String VERYFID = "VERYFID";
+  ;
 }
